24817e79381479e7a29566d372e7bb21c5ba9c18
vikash893 <vikashbhardwaj430@gmail.com>
2026-03-08 20:29:52 +0530

    change in atp

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "vikashbhardwaj430@gmail.com",
-    pass: "zidnpwpmshxnqmup"
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
 
