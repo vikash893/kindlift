09667e430aeb33b4b3cc361ef220298088511fe6
vikash893 <vikashbhardwaj430@gmail.com>
2026-04-05 15:06:18 +0530

    server update

--- backend/routes/location.js
+++ backend/routes/location.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 
 const router = express.Router();
 
@@ -27,4 +27,4 @@ router.get("/search", async (req, res) => {
   }
 });
 
-export default router;
+module.exports = router;
