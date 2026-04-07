b59cf740b061f2d5583c8c6690be1241ce21f284
Yashgupta7877 <yashguptamkh9571@gmail.com>
2026-04-07 17:50:12 +0530

    first

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -64,7 +64,10 @@ const otpStore = {};
 
 // mail config
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  //service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
