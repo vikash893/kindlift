acf4c1b04b9ab0c91ab1d4eb5ea638bf044a636e
Vishnu Singh <vishnu.singh_cs.h24@gla.ac.in>
2026-04-11 19:51:42 +0530

    fix: remove white strip above navbar - set html bg to black instead of modifying navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -75,7 +75,7 @@ export const Navbar = () => {
       <nav
         className={`fixed top-0 left-0 right-0 z-[100] transition-all duration-500 ${
           hidden && !mobileOpen ? '-translate-y-full' : 'translate-y-0'
-        } ${scrolled ? 'py-2' : 'pt-2 pb-4'}`}
+        } ${scrolled ? 'py-2' : 'py-5'}`}
       >
         <div className="max-w-7xl mx-auto px-6 lg:px-8">
           <div
